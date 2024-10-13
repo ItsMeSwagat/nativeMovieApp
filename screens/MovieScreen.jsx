823982199cb107ec1dc76c71caced951323dc105
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-10-13 23:19:00 +0545

    movie details tmdb API fetch and displayed

--- screens/MovieScreen.jsx
+++ screens/MovieScreen.jsx
@@ -16,6 +16,12 @@ import { LinearGradient } from "expo-linear-gradient";
 import Cast from "../components/cast";
 import MovieList from "../components/movieList";
 import Loading from "../components/loading";
+import {
+  fetchMovieCredits,
+  fetchMovieDetails,
+  fetchSimilarMovies,
+  img500,
+} from "../api/movieDB";
 
 const { width, height } = Dimensions.get("window");
 const ios = Platform.OS == "ios";
@@ -25,12 +31,41 @@ export default function MovieScreen() {
   const { params: item } = useRoute();
   const navigation = useNavigation();
   const [isFavourite, setIsFavourite] = useState(false);
-  const [cast, setCast] = useState([1, 2, 3, 4, 5]);
-  const [similarMovies, setSimilarMovies] = useState([1, 2, 3, 4, 5]);
+  const [cast, setCast] = useState([]);
+  const [similarMovies, setSimilarMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [movie, setMovie] = useState({});
   let movieName = "Harold and the Purple Crayon";
 
-  useEffect(() => {}, item);
+  useEffect(() => {
+    setLoading(true);
+    getMovieDetails(item.id);
+    getMovieCredits(item.id);
+    getSimilarMovies(item.id);
+  }, item);
+
+  const getMovieDetails = async (id) => {
+    const data = await fetchMovieDetails(id);
+    if (data) {
+      setMovie(data);
+    }
+    setLoading(false);
+  };
+
+  const getMovieCredits = async (id) => {
+    const data = await fetchMovieCredits(id);
+    if (data && data.cast) {
+      setCast(data.cast);
+    }
+  };
+
+  const getSimilarMovies = async (id) => {
+    const data = await fetchSimilarMovies(id);
+    if (data && data.results) {
+      setSimilarMovies(data.results);
+    }
+  };
+
   return (
     <>
       {loading ? (
@@ -65,8 +100,8 @@ export default function MovieScreen() {
 
             <View>
               <Image
-                source={require("../assets/poster1.jpg")}
-                style={{ width, height: height * 0.55 }}
+                source={{ uri: img500(movie?.poster_path) }}
+                style={{ width, height: height * 0.65 }}
               />
               <LinearGradient
                 colors={[
@@ -86,18 +121,31 @@ export default function MovieScreen() {
 
           <View style={{ marginTop: -(height * 0.03) }} className=" space-y-3">
             <Text className=" text-white text-center text-3xl font-semibold tracking-wider px-4">
-              {movieName}
-            </Text>
-            <Text className=" text-neutral-400 text-base text-center font-semibold">
-              Released * 2020 * 170 min
+              {movie?.title}
             </Text>
 
-            <View className=" flex-row justify-center mx-4 space-x-2">
+            {movie?.id ? (
               <Text className=" text-neutral-400 text-base text-center font-semibold">
-                Action *
+                {movie?.status} • {movie?.release_date?.split("-")[0]} •{" "}
+                {movie?.runtime} min
               </Text>
+            ) : null}
+
+            <View className=" flex-row justify-center mx-4 space-x-2">
+              {movie?.genres?.map((genre, index) => {
+                let showDot = index + 1 != movie.genres.length;
+                return (
+                  <Text
+                    key={index}
+                    className=" text-neutral-400 text-base text-center font-semibold"
+                  >
+                    {genre?.name} {showDot ? "•" : null}
+                  </Text>
+                );
+              })}
+
               <Text className=" text-neutral-400 text-base text-center font-semibold">
-                Thrill *
+                Thrill •
               </Text>
               <Text className=" text-neutral-400 text-base text-center font-semibold">
                 Comedy
@@ -105,31 +153,7 @@ export default function MovieScreen() {
             </View>
 
             <Text className=" text-neutral-400 mx-4 tracking-wide text-justify">
-              There's something about Zachary Levi that I quite like. Like in
-              "Shazam" (2019), he has a boyish charm to him that is engaging to
-              watch. In this, admittedly the story is all pretty thin, but his
-              characterisation has a friendliness to it that I found quite
-              enjoyable. "Harold" is the human manifestation of a fellow who
-              lives quite happily in the land of fiction, but who arrives in the
-              real world to try and find his creator "the old man". Together
-              with his more reluctant pal "Moose" (Lil Rel Howery) he starts
-              approaching old gents in the park - and yes, well that just causes
-              pain. Then he encounters the young "Mel" (Benjamin Bottani) and
-              his mum "Terry" (Zooey Deschanel) after she hits him with her car.
-              An overnight at their home sees the start of a rather daft bonding
-              exercise that's completely devoid of jeopardy but thanks to the
-              odd intervention from fantasy-obsessed librarian "Gary" (Jermaine
-              Clement) and his straggling pal "Porcupine" (Tanya Reynolds) is
-              quite fun. There's an innocence to the whole thing that is quite
-              touchingly simple and honest. People with invisible friends -
-              didn't we all? The animation is perfectly adequate and the purple
-              crayon could teach a 3D printer a thing or two about creating an
-              aeroplane or a car tyre. Sure, it's all forgettable fayre and I'm
-              not too sure if it's in any way sophisticated enough for today's
-              kids - but it's generally quite an entertaining vehicle for a star
-              who looks every inch in his element. It's a summer movie that's as
-              good as any of the more hyped animation's I've seen so far in
-              2024.
+              {movie?.overview}
             </Text>
           </View>
 
